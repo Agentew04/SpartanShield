93ef0a6404f36b85b2d1c1414c1586b008c66e95
Rodrigo Appelt <digoappelt@gmail.com>
2022-02-12 21:03:05 -0300

    created binary extension

---
+++ src/BinaryExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartanShield
+{
+    public static class BinaryExtensions
+    {
+        /// <summary>
+        /// Reads a Guid from a stream
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <returns>The Guid that has been read</returns>
+        public static Guid ReadGuid(this BinaryReader reader) => new(reader.ReadBytes(16));
+
+        /// <summary>
+        /// Writes a Guid to a stream
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="guid">The Guid that will be written</param>
+        public static void Write(this BinaryWriter writer, Guid guid) => writer.Write(guid.ToByteArray());
+
+        public static CryptoItem ReadCryptoItem(this BinaryReader reader)
+        {
+            Guid id = reader.ReadGuid();
+            Guid owner = reader.ReadGuid();
+            Guid parentDrive = reader.ReadGuid();
+            bool isEncrypted = reader.ReadBoolean();
+            string path = reader.ReadString();
+
+            var filesCount = reader.ReadInt32();
+            var files = new List<Guid>();
+            for (int i = 0; i < filesCount; i++) files.Add(reader.ReadGuid());
+
+            return new()
+            {
+                Id = id,
+                Owner = owner,
+                ParentDrive = parentDrive,
+                IsEncrypted = isEncrypted,
+                Path = path,
+                Files = files
+            };
+        }
+
+        public static void WriteCryptoItem(this BinaryWriter writer, CryptoItem item)
+        {
+            writer.Write(item.Id);
+            writer.Write(item.Owner);
+            writer.Write(item.ParentDrive);
+            writer.Write(item.IsEncrypted);
+            writer.Write(item.Path);
+            writer.Write(item.Files.Count);
+            foreach (var file in item.Files) writer.Write(file);
+        }
+    }
+}
--- src/CryptoItem.cs
+++ src/CryptoItem.cs
@@ -6,12 +6,35 @@ namespace SpartanShield
 {
     public class CryptoItem
     {
+        /// <summary>
+        /// The <see cref="Guid"/> of the user that is the owner of this file
+        /// </summary>
         public Guid Owner { get; set; }
+
+        /// <summary>
+        /// The <see cref="Guid"/> of this item
+        /// </summary>
         [BsonId]
         public Guid Id { get; set; }
+
+        /// <summary>
+        /// The <see cref="Guid"/> of where the item is located(PC/USB)
+        /// </summary>
         public Guid ParentDrive { get; set; }
+
+        /// <summary>
+        /// If this item is currently encrypted
+        /// </summary>
         public bool IsEncrypted { get; set; }
-        public string? Path { get; set; }
+
+        /// <summary>
+        /// The path of the original file
+        /// </summary>
+        public string Path { get; set; } = string.Empty;
+
+        /// <summary>
+        /// A list of file guids that belongs to this item
+        /// </summary>
         public List<Guid> Files { get; set; } = new();
     }
 }
